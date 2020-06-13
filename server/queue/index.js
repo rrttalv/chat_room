c35a1f2347549d0104c64bf055fa32f8e4c8931d
Rico Rein Talvar <ricotalvar@gmail.com>
2020-06-13 13:59:38 +0300

    on disconnect user leave public namespace

--- server/queue/index.js
+++ server/queue/index.js
@@ -16,18 +16,29 @@ saveQueue.process((job, done) => {
 });
 
 const activeUsers = [];
+const socketIDs = [];
 
 export default io => {
     io.on('connection', socket => {
         socket.on('subscribe', data => {
+            const { id } = socket;
             const { room, user } = data;
             socket.join(room);
-            console.log(user)
+            user.socketID = id;
             if(activeUsers.indexOf(user) === -1){
                 activeUsers.push(user);
             }
             io.in(room).emit('public_join', {list: activeUsers});
         });
+        socket.on('disconnect', () => {
+            const { id } = socket;
+            const userToDisconnect = activeUsers.find(({socketID}) => socketID === id);
+            const idx = activeUsers.indexOf(userToDisconnect);
+            if(idx !== -1){
+                activeUsers.splice(idx, 1);
+            }
+            io.in('public-room').emit('public_join', {list: activeUsers})
+        })
         socket.on('message', data => {
             saveQueue.add(data);
         });
